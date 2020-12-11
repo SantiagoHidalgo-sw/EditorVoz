ebf8b0d67db6038fcf237e6967fa60de3b1625b6
santiago <santiago.hida@gmail.com>
2020-12-11 19:57:59 -0300

    vistas

--- EditorVoz/ViewController.swift
+++ EditorVoz/ViewController.swift
@@ -1,19 +1,40 @@
-//
-//  ViewController.swift
-//  EditorVoz
-//
-//  Created by Santiago Hidalgo on 11/12/2020.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dejarGrabar.isEnabled = false
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("se llamo a la funcion")
     }
 
-
+    @IBOutlet weak var etiquetaGrabado: UILabel!
+    @IBOutlet weak var dejarGrabar: UIButton!
+    @IBOutlet weak var grabarAudio: UIButton!
+    
+    
+    
+    @IBAction func grabarAudio(_ sender: Any) {
+        etiquetaGrabado.text = "Grabando.."
+        dejarGrabar.isEnabled = true
+        grabarAudio.isEnabled = false
+    }
+    
+    @IBAction func dejarGrabar(_ sender: Any) {
+        etiquetaGrabado.text = "Presione para grabar"
+        dejarGrabar.isEnabled = false
+        grabarAudio.isEnabled = true
+        
+    }
+    
 }
 
