cca3b856cefe6b2d7c2aec62b4e4c5e19314a122
santiago <santiago.hida@gmail.com>
2020-12-13 11:24:28 -0300

    final sin vistas

--- EditorVoz/PlaySoundsViewController.swift
+++ EditorVoz/PlaySoundsViewController.swift
@@ -1,14 +1,47 @@
 
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
     var recordedAudioURL: URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
 
+    enum ButtonType: Int { case fast = 0, slow }
+    
+    @IBOutlet weak var botonRapido: UIButton!
+    @IBOutlet weak var botonLento: UIButton!
+    @IBOutlet weak var botonParar: UIButton!
+
+    
+    @IBAction func reproducirSonido (_sender: UIButton){
+        switch(ButtonType(rawValue: _sender.tag)!) {
+           case .fast:
+               playSound(rate: 1.5)
+           case .slow:
+               playSound(rate: 0.5)
+           
+           }
+
+           configureUI(.playing)
+    }
+    @IBAction func pararSonido (_sender: UIButton){
+        print("manteca")
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupAudio()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configureUI(.notPlaying)
     }
 
 
